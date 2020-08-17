d1e9758f19cc7aa46e5a780256dd1130c39f19d9
Glen Hertz <glen.hertz@gmail.com>
2020-08-17 00:08:27 -0400

    Switch to slurping output from 1 ping process per ip
    Hopefully this will be more reliable and use less system/network
    resources.  Can still cause issues if too many IPs are requested.

--- src/Ping.jl
+++ src/Ping.jl
@@ -1,74 +1,106 @@
 module Ping
 
-export ping, pping, monitor_subnet, ip_range, ip2tuple, sort_ips, monitor_active, monitor_ips
+export ping, monitor_subnet, ip_range, ip2tuple, ping_active, monitor_ips, skip_bad_hosts
 
 using Dates, Base.Iterators, Sockets
 
 
-"""
-    ping(ips::AbstractArray; c=1, W=0.1, loss=-0.001, verbose=false, sleeper=()->0)
-
-ping
-"""
-function ping(ip; c=1, W=0.1, loss=-1, verbose=false)
-   try
-       s = read(`ping -c $c -W $W $ip`, String)
-       m = match(r"([0-9.]+)% packet loss, time (\d.*)+\nrtt min/avg/max/mdev = ([0-9.]+)/([0-9.]+)/([0-9.]+)/([0-9.]+)\s+(\S+)", s)
-       if !isnothing(m)
-           pct_packet_loss = round(Int, parse(Float64, m[1]))
-           mult = 1e3
-           if m[7] == "ms"
-              mult = 1
-           end
-           #min = round(Int, parse(Float64, m[3]) * mult)
-           #avg = round(Int, parse(Float64, m[4]) * mult)
-           max = round(Int, parse(Float64, m[5]) * mult)
-           #mdev = round(Int, parse(Float64, m[6]) * mult)
-           verbose && println("$ip\t$(pct_packet_loss)%\t$max")
-           return (ip=ip, loss=pct_packet_loss, max=max)
-       else
-           verbose && println("$ip no match: $s")
-           return (ip=ip, loss=100, max=Float64(loss))
-       end
-   catch e
-       if verbose
-           verbose && println("$ip error: $e")
-       end
-       return (ip=ip, loss=100, max=Float64(loss))
-   end
+function ping(results::Channel, jobid::Integer, ip::IPv4; max_ping=999, verbose=false)
+    eachping = Iterators.Stateful(eachline(`ping -D -O -W $(max_ping/1000) $ip`))
+    # Drop first line (header of ping output):
+    line1 = popfirst!(eachping)
+    verbose && println(line1)
+    for line in eachping
+        verbose && println(line)
+        ms = max_ping
+        m = match(r"time\s*[=<]\s*([0-9]+(\.[0-9]+)?)\s*ms", line)
+        if !isnothing(m) 
+            ms = min(max_ping, round(Int, parse(Float64, m[1])))
+        end
+        put!(results, (jobid, ms))
+    end
 end
 
-"""
-    pping(ips::AbstractArray; c=1, W=0.1, max_default=5000.0, verbose=false, sleeper=()->0)
-
-Parallel ping
-"""
-function pping(ips::AbstractArray; c=1, W=0.1, loss=999, verbose=false, sleeper=()->0)
+function ping(ips::AbstractVector{IPv4}; run_for::TimePeriod=Second(10), update_interval::TimePeriod=Millisecond(1100), max_ping=999, verbose=false, log=true, summarize=false)
+    results = Channel{Tuple{Int64, Int64}}(100)
+    for (jobid, ip) in enumerate(ips)
+        @async ping(results, jobid, ip; max_ping, verbose)
+    end
     N = length(ips)
-    ss = Vector{Any}(undef, N)
-    @sync begin
-        for i in 1:N
-            ip = ips[i]
-            @async begin
-                sleep(sleeper())
-                p = ping(ip; c=c, W=W, loss=loss, verbose=verbose)
-                if !isnothing(p) && typeof(p.max) <: Real
-                    ss[i] = p
-                else
-                    #ss[i] = missing
+    latest_pings = fill(-1, N)
+    max_pings = fill(-1, N)
+    min_pings = fill(max_ping, N)
+    sum_pings = fill(0, N)
+    count = 0
+    if summarize
+        println("Info: Monitoring these $N IPs for $run_for:")
+        summary = ip_summary(ips)
+        print(summary)
+    end
+    start = now()
+    fname = "ping_$(start).csv"
+    f = open(fname, "w")
+    try
+        update_time = start + update_interval
+        str = string(rpad("Time", 23), ",", join(lpad.(alias.(ips), 3), ","))
+        if log
+            println(str)
+            println(f, str)
+        end
+        while true
+            jobid, ms = take!(results)
+            latest_pings[jobid] = ms
+            if now() > update_time #&& all(>(0), latest_pings)
+                for i in 1:N
+                    if latest_pings[i] == -1
+                        continue
+                    end
+                    max_pings[i] = max(max_pings[i], latest_pings[i])
+                    min_pings[i] = min(min_pings[i], latest_pings[i])
+                    sum_pings[i] += latest_pings[i]
                 end
+                count += 1
+                rm_negatives = [p == -1 ? lpad("", 3) : lpad(p, 3) for p in latest_pings]
+                str = string(rpad(now(), 23, "0"), ",", join(rm_negatives, ","))
+                if log
+                    if summarize && count % 30 == 0
+                        println(summary)
+                    end
+                    println(str)
+                    println(f, str)
+                end
+                update_time = now() + update_interval
+            end
+            if now() > start + run_for
+                break
             end
         end
+    finally
+        close(f)
+        close(results)
     end
-    return ss
+    if count == 0
+        return []
+    end
+    avg_pings = round.(Int, sum_pings ./ count)
+    return [(min=min_pings[i], avg=avg_pings[i], max=max_pings[i]) for i in 1:N]
+end
+
+function ping(ips::AbstractVector{<:AbstractString}; kwargs...)
+    ping(getaddrinfo.(ips); kwargs...)
+end
+function ping(ip; kwargs...)
+    ping([ip]; kwargs...)
+end
+function ping(byte1, byte2, byte3, byte4; kwargs...)
+    ping(ip_range(byte1, byte2, byte3, byte4); kwargs...)
 end
 
 const T = Union{<:Int,<:OrdinalRange{Int64,Int64}}
-
 function ip_range(byte1::T, byte2::T, byte3::T, byte4::T)
     # Reverse 'product' so fastest iteration index is the leftmost byte
     ips = vec(collect(product(byte4, byte3, byte2, byte1)))
-    ips = [join(ip[end:-1:1], ".") for ip in ips]
+    ips = getaddrinfo.([join(ip[end:-1:1], ".") for ip in ips])
 end
 
 function ip2tuple(ip::AbstractString)
@@ -77,92 +109,68 @@ end
 function tuple2ip(ipt::NTuple{4, Int64})
     join(ipt, ".")
 end
-function sort_ips(ips::AbstractVector{<:AbstractString})
-    tuple2ip.(sort(ip2tuple.(ips)))
+function host(ip::IPv4)
+    names = getnameinfo.(ip)
+    host = first(split(string(names), "."))
 end
-function ip_summary(ips)
-    lns = ip2long_name.(ips)
-    sns = long2short_name.(lns)
-    als = short_host2alias.(sns)
+function alias(ip::IPv4)
+    name = host.(ip)
+    string(name[1], name[2], name[end])
+end
+    
+function ip_summary(ips::AbstractVector{IPv4})
+    lns = getnameinfo.(ips)
+    sns = host.(ips)
+    als = alias.(ips)
     str = ""
     for (ip, ln, sn, al) in zip(ips, lns, sns, als)
         str *= string("    $(rpad(ip, 15)) $(rpad(ln, 30)) $(rpad(sn, 15)) $(lpad(al, 3))\n")
     end
     return str
 end
-
-function skip_bad_hosts(ips::AbstractVector; init_pings=10, c=1, W=0.1, loss=999, verbose=false, sleeper=()->0)
-    println("Info: Scanning $(length(ips)) hosts to skip ones with 100% packet loss for $init_pings consecutive pings")
-    if init_pings > 0
-        active_ips = Set{String}()
-        for i in 1:init_pings
-            pings = pping(ips::AbstractArray; c, W, loss, verbose, sleeper)
-            actives = [i.ip for i in filter(p->p.loss != 100, pings)]
-            union!(active_ips, actives)
-        end
-        skips = setdiff(ips, active_ips)
-        ips = sort_ips(collect(active_ips))
-        if length(skips) > 0
-            println("Warning: Skipping $(length(skips)) IPs with 100% packet loss after $init_pings pings")
-            println("Info: Monitoring these $(length(ips)) IPs:")
-            lns = ip2long_name.(ips)
-            sns = long2short_name.(lns)
-            print(ip_summary(ips))
-        end
-    end
-    ips = sort_ips(ips)
+function ip_summary(ips::AbstractVector{<:AbstractString})
+    ip_summary(getaddrinfo.(ips))
+end
+function ip_summary(ip)
+    ip_summary([ips])
 end
 
-function monitor_active(byte1::T, byte2::T, byte3::T, byte4::T, period=Minute(1); init_pings=10, repeat_header=30, c=1, W=0.1, loss=999, verbose=false, sleeper=()->0)
+function skip_bad_hosts(ips::AbstractVector{IPv4}; run_for::TimePeriod=Second(10), max_ping=999, verbose=false, log=false, summarize=false)
+    N = length(ips)
+    println("Info: Scanning $(N) hosts for $run_for to skip ones with 100% packet loss")
+    stats = ping(ips; run_for, verbose, log, summarize)
+    #foreach(println, stats)
+    skips = ips[(filter(i -> stats[i].min == max_ping, 1:N))]
+    setdiff!(ips, skips)
+    if length(skips) > 0
+        println("Warning: Skipping $(length(skips)) IPs with 100% packet loss after $run_for")
+    end
+    return ips
+end
+function skip_bad_hosts(ips::AbstractVector{<:AbstractString}; kwargs...)
+    ips = getaddrinfo.(ips)
+    skip_bad_hosts(ips; kwargs...)
+end
+function skip_bad_hosts(byte1, byte2, byte3, byte4; kwargs...)
     ips = ip_range(byte1, byte2, byte3, byte4)
-    ips = skip_bad_hosts(ips; init_pings, W, c, loss, verbose, sleeper)
-    montior_ips(ips, period; repeat_header, c, W, loss, verbose, sleeper)
-    ips 
+    skip_bad_hosts(ips; kwargs...)
 end
-function short_host2alias(short_name::AbstractString)
-    if startswith(short_name, r"[0-9]")
-        return last(split(short_name, "."))
-    else
-        return string(short_name[1], short_name[2], short_name[end])
-    end
+
+function ping_active(ips::AbstractVector{IPv4}; run_for=Second(10), skip_time::TimePeriod=Second(10), repeat_header=30, max_ping=999, log=true, log_skips=false, verbose=false, summarize=true)
+    ips = skip_bad_hosts(ips; log=log_skips, run_for=skip_time, verbose)
+    stats = ping(ips; run_for, max_ping, verbose, log, summarize)
+    [(ip=ips[i], min=stats[i].min, stats[i].avg, stats[i].max) for i in 1:length(ips)]
 end
-function long2short_name(long_name::AbstractString)
-    if startswith(long_name, r"[0-9]")
-        return long_name
-    else
-        return first(split(long_name, '.'))
-    end
+function ping_active(ips::AbstractVector{<:AbstractString}; kwargs...)
+    ips = getaddrinfo.(ips)
+    ping_active(ips; kwargs...)
 end
-function ip2long_name(ip)
-    long_names = getnameinfo(IPv4(ip))
+function ping_active(byte1::T, byte2::T, byte3::T, byte4::T; kwargs...)
+    ips = ip_range(byte1, byte2, byte3, byte4)
+    ping_active(ips; kwargs...)
 end
-function montior_ips(ips, period=Minute(1); repeat_header=30, c=1, W=1, loss=999, verbose=false, sleeper=()->0)
-    start = now()
-    fname = "ping_$(start).csv"
-    long_names = ip2long_name.(ips)
-    short_names = long2short_name.(long_names)
-    aliases = short_host2alias.(short_names)
-    alias_header = string(rpad("DateTime",23), ",", join([lpad(al, 3) for al in aliases], ","))
-    open(fname, "w") do f
-        println(f, alias_header)
-        println(alias_header)
-        ipst = ip2tuple.(ips)
-        ip_sum = ip_summary(ips)
-        count = 0
-        while now() < start + period
-            count += 1
-            if count % repeat_header == 0
-                print(ip_sum)
-                println(alias_header)
-            end
-            pings = pping(ips; c, W, loss, verbose, sleeper)
-            pings = sort(pings, by=x->ip2tuple(x.ip))
-            str = string(rpad(now(), 23, "0"), ",", join([lpad(p.max, 3) for p in pings], ","))
-            println(f, str)
-            println(str)
-        end
-    end
-    ips
+function ping_active(host; kwargs...)
+    ping_active([host]; kwargs...)
 end
 
 
